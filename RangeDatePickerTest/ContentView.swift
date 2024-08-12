890625ba0be685a21f8d83be81b2dea52eb83d0f
Musk <inhoyoo1029@gmail.com>
2024-08-12 14:36:08 +0900

    Initial Commit

---
+++ RangeDatePickerTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RangeDatePickerTest
+//
+//  Created by 에스지랩 on 8/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RangeDatePickerTest/RangeDatePickerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RangeDatePickerTestApp.swift
+//  RangeDatePickerTest
+//
+//  Created by 에스지랩 on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RangeDatePickerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
